3e83cc0b0af3c786139395632d37da6a8e05cd9e
ataokaman <taisei.takaoka@icloud.com>
2026-03-26 19:04:44 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,39 +1,19 @@
 (function(){
   console.log("もてょBOT起動");
 
-  function getInputFromDoc(doc){
-    return doc.querySelector('[contenteditable="true"], textarea, input');
-  }
-
-  function findInput(){
-    // ① 自分のページ
-    let input = getInputFromDoc(document);
-    if(input) return input;
-
-    // ② iframeの中を全部探す
-    let iframes = document.querySelectorAll("iframe");
-
-    for(let iframe of iframes){
-      try{
-        let doc = iframe.contentDocument || iframe.contentWindow.document;
-        let input = getInputFromDoc(doc);
-        if(input) return input;
-      }catch(e){
-        // クロスオリジンは無視
-      }
-    }
-
-    return null;
+  function getInput(){
+    return document.querySelector('[contenteditable="true"], textarea, input');
   }
 
   function send(text){
-    const input = findInput();
+    const input = getInput();
 
     if(!input){
-      console.log("入力欄見つからない");
+      console.log("入力欄なし");
       return;
     }
 
+    // 入力
     if(input.isContentEditable){
       input.innerText = text;
     } else {
@@ -42,16 +22,18 @@
 
     input.dispatchEvent(new Event('input', { bubbles: true }));
 
+    // 🔥 Ctrl + Enterで送信
     input.dispatchEvent(new KeyboardEvent('keydown', {
       bubbles: true,
       cancelable: true,
       key: 'Enter',
-      code: 'Enter'
+      code: 'Enter',
+      ctrlKey: true
     }));
   }
 
   setTimeout(()=>{
     send("もてょBOT起動");
-  },1500);
+  },1000);
 
 })();
